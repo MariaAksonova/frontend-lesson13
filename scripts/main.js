0dcf5cb4cbba82dbd79dbc1a9d5a3ac370bed420
Maria Aksonova <mashaaks@ukr.net>
2017-01-18 19:46:21 +0200

    SCSS

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
-/**
- * Created by Masha on 14.01.2017.
- */
+$(function (){
+    $('.new-generation .main-btn').on('click', function () {
+        $('.new-generation p').fadeIn(1000);
+        $('.new-generation h3').css('font-size','72px');
+    });
+    $('.products .main-btn').on('click', function () {
+        $('.products p').fadeIn(1000);
+        $('.products h3').css('font-size','72px');
+    });
+    $('.start-journey .main-btn').on('click', function () {
+        $('.start-journey p').fadeIn(1000);
+        $('.start-journey h3').css('font-size','72px');
+    });
+
+});
